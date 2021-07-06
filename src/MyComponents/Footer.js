5503e76579dd6f9b67a8b895a65a6953a3158b32
apm191 <apurv.mundhra@gmail.com>
2021-07-06 12:13:36 +0530

    initial commit

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Footer = () => {
+    // let footerStyle = {
+    //     width: "100%",
+    //     position: "relative",
+    //     top: "100vh",
+    // }
+    return (
+        <footer className="bg-dark text-light py-3" >
+            <p className="text-center">
+                Copyright &copy; MyTodosList.com
+            </p>
+        </footer>
+    )
+}
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Todoitem } from "./Todoitem";
+
+export const Todos = (props) => {
+
+    let myStyle = {
+        minHeight: "70vh",
+        margin: "50px auto"
+    }
+
+    return (
+        <div className="container" style={myStyle}>
+            <h3 className="text-center my-3">Todos List </h3>
+
+            {props.todos.length == 0 ? "No Todos to Display" :
+                props.todos.map((todo) => {
+                    return (
+                        <>
+                            <Todoitem todo={todo} key={todo.sno} onDelete={props.onDelete} />
+                            <hr />
+                        </>
+                    )
+                })
+            }
+        </div>
+    )
+}
